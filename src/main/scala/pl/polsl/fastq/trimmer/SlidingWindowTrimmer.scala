c648c4dd3853eb8d500087629a6085470049453d
grobocop12 <chasnik97876@gmail.com>
2023-04-10 20:11:15 +0200

    small refactor

--- src/main/scala/pl/polsl/fastq/trimmer/SlidingWindowTrimmer.scala
+++ src/main/scala/pl/polsl/fastq/trimmer/SlidingWindowTrimmer.scala
@@ -7,42 +7,11 @@ import scala.annotation.tailrec
 import scala.util.control.Breaks.break
 
 class SlidingWindowTrimmer(windowLength: Int, requiredQuality: Float) extends Trimmer {
-  val totalRequiredQuality: Float = windowLength * requiredQuality
+  private val totalRequiredQuality: Float = windowLength * requiredQuality
 
-  override def apply(in: RDD[FastqRecord]): RDD[FastqRecord] = in.map(trim_asdf).filter(_ != null)
+  override def apply(in: RDD[FastqRecord]): RDD[FastqRecord] = in.map(trim).filter(_ != null)
 
   private def trim(in: FastqRecord): FastqRecord = {
-    val quals = in.qualityAsInteger
-    if (in.sequence.length < windowLength) return null
-    var total = quals.take(windowLength).sum
-    if (total < totalRequiredQuality) return null
-    var lengthToKeep = quals.length
-    for (i <- 0 to (quals.length - windowLength)) {
-      total = total - quals(i) + quals(i + windowLength)
-      if (total < totalRequiredQuality) {
-        lengthToKeep = i + windowLength
-        break
-      }
-    }
-    var i = lengthToKeep
-    var lastBaseQuality = quals(i - 1)
-    while (lastBaseQuality < requiredQuality && i > 1) {
-      i -= 1
-      lastBaseQuality = quals(i - 1)
-    }
-    if (i < 1) null
-    if (i < quals.length) FastqRecord(in.name, in.sequence.substring(0, i), in.comment, in.quality)
-    in
-  }
-
-  private def trim_asdf(in: FastqRecord): FastqRecord = {
-    @tailrec
-    def calculateLength(qualsSums: Iterator[Int], lengthToKeep: Int): Int = {
-      if (!qualsSums.hasNext || qualsSums.next() < totalRequiredQuality) {
-        return lengthToKeep
-      }
-      calculateLength(qualsSums, lengthToKeep + 1)
-    }
 
     if (in.sequence.length < windowLength) return null
     val qualitySums = in.qualityAsInteger.sliding(windowLength).map(_.sum)
@@ -52,5 +21,13 @@ class SlidingWindowTrimmer(windowLength: Int, requiredQuality: Float) extends Tr
       val lengthToKeep = calculateLength(qualitySums, windowLength)
       FastqRecord(in.name, in.sequence.substring(0, lengthToKeep), in.comment, in.quality.substring(0, lengthToKeep), in.phredOffset)
     }
+  }
+
+  @tailrec
+  private def calculateLength(qualsSums: Iterator[Int], lengthToKeep: Int): Int = {
+    if (!qualsSums.hasNext || qualsSums.next() < totalRequiredQuality) {
+      return lengthToKeep
+    }
+    calculateLength(qualsSums, lengthToKeep + 1)
   }
 }
