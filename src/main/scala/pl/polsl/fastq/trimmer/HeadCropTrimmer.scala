8f9073c9f545bc0cda1e21f9b957dca253f53fa3
grobocop12 <chasnik97876@gmail.com>
2023-04-10 20:11:25 +0200

    small refactor

--- src/main/scala/pl/polsl/fastq/trimmer/HeadCropTrimmer.scala
+++ src/main/scala/pl/polsl/fastq/trimmer/HeadCropTrimmer.scala
@@ -3,12 +3,12 @@ package pl.polsl.fastq.trimmer
 import org.apache.spark.rdd.RDD
 import pl.polsl.fastq.data.FastqRecord
 
-class HeadCropTrimmer(bases: Int) extends Trimmer {
+class HeadCropTrimmer(length: Int) extends Trimmer {
   override def apply(in: RDD[FastqRecord]): RDD[FastqRecord] = in.map(this.headCrop).filter(_ != null)
 
   private def headCrop(rec: FastqRecord): FastqRecord = {
-    if (bases >= rec.sequence.length)
+    if (length >= rec.sequence.length)
       null
-    else FastqRecord(rec.name, rec.sequence.substring(bases), rec.comment, rec.quality.substring(bases), rec.phredOffset)
+    else FastqRecord(rec.name, rec.sequence.substring(length), rec.comment, rec.quality.substring(length), rec.phredOffset)
   }
 }
