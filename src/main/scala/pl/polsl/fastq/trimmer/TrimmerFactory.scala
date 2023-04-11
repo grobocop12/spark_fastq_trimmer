53f7e05887cbacde957ecfe9ba9c7905a24fd31c
grobocop12 <chasnik97876@gmail.com>
2023-04-11 19:45:00 +0200

    TrimmerFactory updated

--- src/main/scala/pl/polsl/fastq/trimmer/TrimmerFactory.scala
+++ src/main/scala/pl/polsl/fastq/trimmer/TrimmerFactory.scala
@@ -11,15 +11,18 @@ object TrimmerFactory {
     name match {
       case "CROP" => new CropTrimmer(args.toInt)
       case "HEADCROP" => new HeadCropTrimmer(args.toInt)
-      case "LEADING" => new LeadingTrimmer(args.toInt )
+      case "LEADING" => new LeadingTrimmer(args.toInt)
       case "MINLEN" => new MinLenTrimmer(args.toInt)
+      case "MAXINFO" =>
+        val splittedArgs = args.split(":")
+        new MaximumInformationTrimmer(splittedArgs(0).toInt, splittedArgs(1).toFloat)
       case "MAXLEN" => new MaxLenTrimmer(args.toInt)
       case "SLIDINGWINDOW" =>
         val splittedArgs = args.split(":")
         new SlidingWindowTrimmer(splittedArgs(0).toInt, splittedArgs(1).toFloat)
       case "TAILCROP" => new TailCropTrimmer(args.toInt)
       case "LEADING" => new LeadingTrimmer(args.toInt)
-      case "TRAILING" => new TrailingTrimmer(args.toInt )
+      case "TRAILING" => new TrailingTrimmer(args.toInt)
       case "TOPHRED33" => new ToPhred33Trimmer()
       case "TOPHRED64" => new ToPhred64Trimmer()
       case _ => throw new RuntimeException(s"Unknown trimmer: $name")
