e2f2b335f4b6772e7d3d13a93e9801a73aa7a39e
grobocop12 <chasnik97876@gmail.com>
2023-04-14 20:36:56 +0200

    Added FastaParser and FastaRecord

--- src/main/scala/pl/polsl/fastq/trimmer/IlluminaClippingTrimmer.scala
+++ src/main/scala/pl/polsl/fastq/trimmer/IlluminaClippingTrimmer.scala
@@ -1,9 +1,10 @@
 package pl.polsl.fastq.trimmer
 
 import org.apache.spark.rdd.RDD
-import pl.polsl.fastq.data.FastqRecord
+import pl.polsl.fastq.data.{FastaRecord, FastqRecord}
 import pl.polsl.fastq.illumina.{IlluminaClippingSeq, IlluminaPrefixPair}
-import pl.polsl.fastq.trimmer.IlluminaClippingTrimmer.LOG10_4
+import pl.polsl.fastq.trimmer.IlluminaClippingTrimmer.{LOG10_4, PREFIX, SUFFIX_F, SUFFIX_R}
+import pl.polsl.fastq.utils.FastaParser
 
 import java.io.File
 import java.util
@@ -15,7 +16,6 @@ class IlluminaClippingTrimmer(val seedMaxMiss: Int,
                               val minPrefix: Int,
                               val palindromeKeepBooth: Boolean) extends Trimmer {
   val minSequenceOverlap = calculateMinSequenceOverlap()
-
   val prefixPairs = new mutable.ListBuffer[IlluminaPrefixPair]()
   val commonSeqs = new mutable.HashSet[IlluminaClippingSeq]()
   val forwardSeqs = new mutable.HashSet[IlluminaClippingSeq]()
@@ -27,10 +27,40 @@ class IlluminaClippingTrimmer(val seedMaxMiss: Int,
     val result = (minSequenceLikelihood / LOG10_4).toInt
     if (result > 15) 15 else result
   }
+
+  private def loadSequences(sequencePath: File): Unit = {
+    val parser = new FastaParser(sequencePath)
+    val forwardSeqMap = new mutable.HashMap[String, FastaRecord]
+    val reverseSeqMap = new mutable.HashMap[String, FastaRecord]
+    val commonSeqMap = new mutable.HashMap[String, FastaRecord]
+    val forwardPrefix = new mutable.HashSet[String]
+    val reversePrefix = new mutable.HashSet[String]
+    parser.parse()
+    while (parser.hasNext) {
+      val rec = parser.next
+      if (rec.name.endsWith(SUFFIX_F)) {
+        forwardSeqMap.put(rec.name, rec)
+        if (rec.name.startsWith(PREFIX)) forwardPrefix.add(rec.name.substring(0, rec.name.length - SUFFIX_F.length))
+      } else if (rec.name.endsWith(SUFFIX_R)) {
+        reverseSeqMap.put(rec.name, rec)
+        if (rec.name.startsWith(PREFIX)) reversePrefix.add(rec.name.substring(0, rec.name.length - SUFFIX_R.length))
+      } else {
+        commonSeqMap.addOne((rec.name, rec))
+      }
+    }
+    new mutable.HashSet[String].addAll(forwardPrefix)
+      .intersect(reversePrefix)
+//      .map()
+    ???
+  }
 }
 
 object IlluminaClippingTrimmer {
-  private val LOG10_4 = 0.60206f
+  private final val LOG10_4 = 0.60206f
+  private final val SUFFIX_F = "/1"
+  private final val SUFFIX_R = "/2"
+  private final val PREFIX = "Prefix"
+  private final val INTERLEAVE = 4
 
   def createTrimmer(args: Array[String]): IlluminaClippingTrimmer = {
     val file = new File(args(0))
@@ -40,6 +70,7 @@ object IlluminaClippingTrimmer {
     val minPrefix = if (args.length > 4) args(4).toInt else 1
     val palindromeKeepBoth = if (args.length > 5) args(5).toBoolean else false
     val trimmer = new IlluminaClippingTrimmer(seedMaxMiss, minPalindromeLikelihood, minSequenceLikelihood, minPrefix, palindromeKeepBoth)
-    ???
+    trimmer.loadSequences(file)
+    trimmer
   }
 }
--- src/main/scala/pl/polsl/fastq/utils/FastaParser.scala
+++ src/main/scala/pl/polsl/fastq/utils/FastaParser.scala
@@ -1,5 +1,42 @@
 package pl.polsl.fastq.utils
 
-object FastaParser {
+import org.apache.hadoop.shaded.org.jline.utils.InputStreamReader
+import pl.polsl.fastq.data.FastaRecord
+
+import java.io.{BufferedInputStream, BufferedReader, File, FileInputStream}
+
+class FastaParser(val file: File) {
+  val reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(new FileInputStream(file), 1_000_000)))
+  var current: Option[FastaRecord] = None
+  var currentLine: Option[String] = None
+
+  def parse(): Unit = {
+
+  }
+
+  def hasNext: Boolean = ???
+
+  def next: FastaRecord = ???
+
+  def parseOne(): Unit = {
+    current = None
+
+    if (currentLine.isEmpty) currentLine = Option(reader.readLine)
+
+    while (current.nonEmpty && !currentLine.get.startsWith(">")) currentLine = Option(reader.readLine)
+
+    if (currentLine != null && currentLine.startsWith(">")) {
+      val fullName = currentLine.substring(1).trim
+      val tokens = fullName.split("[\\| ]")
+      val name = tokens(0)
+      val builder = new StringBuilder
+      currentLine = reader.readLine
+      while (currentLine != null && !currentLine.startsWith(">")) {
+        if (!currentLine.startsWith(";")) builder.append(currentLine.trim)
+        currentLine = reader.readLine
+      }
+      current = new FastaRecord(name, builder.toString.trim, fullName)
+    }
+  }
 
 }
