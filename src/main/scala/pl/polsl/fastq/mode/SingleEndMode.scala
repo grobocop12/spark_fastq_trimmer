8bd2ef14463a8c2416d3165ea34c09cbff219bb0
grobocop12 <chasnik97876@gmail.com>
2023-04-28 13:31:14 +0200

    Sampling first rows instead of random sample

--- src/main/scala/pl/polsl/fastq/mode/SingleEndMode.scala
+++ src/main/scala/pl/polsl/fastq/mode/SingleEndMode.scala
@@ -14,34 +14,40 @@ class SingleEndMode extends Mode {
   private val PHRED_SAMPLE_SIZE = 100
 
   override def run(argsMap: Map[String, Any]): Unit = {
+    val trimmers = createTrimmers(argsMap("trimmers").asInstanceOf[List[String]])
     val session = SparkSession
       .builder
       .appName(argsMap.getOrElse("appName", "FastTrimmerSE").asInstanceOf[String])
       .master(argsMap.getOrElse("master", "local[*]").asInstanceOf[String])
       .getOrCreate()
     val sc = session.sparkContext
+    sc.setLogLevel("INFO")
 
-    val records = sc.textFile(argsMap("input").asInstanceOf[String])
+    val fastqLines = sc.textFile(argsMap("input").asInstanceOf[String])
       .sliding(4, 4)
+
+    val sample = fastqLines
+      .take(PHRED_SAMPLE_SIZE)
       .map(x => FastqRecord(x(0), x(1), x(2), x(3)))
 
-    val phredOffset: Int = argsMap.getOrElse("phredOffset", PhredDetector(records.takeSample(withReplacement = false,
-      PHRED_SAMPLE_SIZE)))
+    val phredOffset: Int = argsMap.getOrElse("phredOffset", PhredDetector(sample))
       .asInstanceOf[Int]
-    val recordsWithOffset = records.map(f => FastqRecord(f.name, f.sequence, f.comment, f.quality, phredOffset))
-    val trimmers = createTrimmers(argsMap("trimmers").asInstanceOf[List[String]])
 
-    val trimmedRecords = applyTrimmer(recordsWithOffset, trimmers)
+    val records = fastqLines.map(x => FastqRecord(x(0), x(1), x(2), x(3), phredOffset))
 
-    trimmedRecords.saveAsTextFile(argsMap("output").asInstanceOf[String])
+    applyTrimmer(records, trimmers)
+      .coalesce(1)
+      .saveAsTextFile(argsMap("output").asInstanceOf[String])
 
     session.close
   }
 
   @tailrec
-  private def applyTrimmer(records: RDD[FastqRecord], trimmers: List[Trimmer]): RDD[FastqRecord] =
+  private def applyTrimmer(records: RDD[FastqRecord], trimmers: List[Trimmer]): RDD[FastqRecord] = {
     if (trimmers.isEmpty)
       records
-    else
+    else {
       applyTrimmer(trimmers.head(records), trimmers.tail)
+    }
+  }
 }
