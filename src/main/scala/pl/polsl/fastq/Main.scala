8574ab0d23f848394634a257d1f424e19a5ee911
grobocop12 <chasnik97876@gmail.com>
2023-05-11 22:48:16 +0200

    File concatenation.

--- src/main/scala/pl/polsl/fastq/Main.scala
+++ src/main/scala/pl/polsl/fastq/Main.scala
@@ -3,8 +3,11 @@ package pl.polsl.fastq
 import pl.polsl.fastq.mode.{HelpMode, PairedEndMode, SingleEndMode}
 import pl.polsl.fastq.utils.{ArgsParser, UsagePrinter}
 
+import java.util.Date
+
 object Main extends App {
   private def run(args: Array[String]): Unit = {
+    val time = new Date().getTime
     val argsMap = ArgsParser.parse(args)
     val mode = argsMap("mode") match {
       case "SE" => new SingleEndMode()
@@ -13,6 +16,7 @@ object Main extends App {
       case _ => throw new RuntimeException("Unknown mode.")
     }
     mode.run(argsMap)
+    println("Milliseconds passed: " + (new Date().getTime - time))
   }
 
   try {
--- src/main/scala/pl/polsl/fastq/mode/SingleEndMode.scala
+++ src/main/scala/pl/polsl/fastq/mode/SingleEndMode.scala
@@ -8,13 +8,18 @@ import pl.polsl.fastq.trimmer.Trimmer
 import pl.polsl.fastq.trimmer.TrimmerFactory.createTrimmers
 import pl.polsl.fastq.utils.PhredDetector
 
-import java.util.Objects
+import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
+import java.nio.file.{FileSystems, Files}
 import scala.annotation.tailrec
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.matching.Regex
 
 class SingleEndMode extends Mode {
   private val PHRED_SAMPLE_SIZE = 100
 
   override def run(argsMap: Map[String, Any]): Unit = {
+    val output = argsMap("output").asInstanceOf[String]
+    val tempOutput = s"$output/temp"
     val trimmers = createTrimmers(argsMap("trimmers").asInstanceOf[List[String]])
     val session = SparkSession
       .builder
@@ -37,11 +42,12 @@ class SingleEndMode extends Mode {
     val records = fastqLines.map(x => FastqRecord(x(0), x(1), x(2), x(3), phredOffset))
 
     applyTrimmer(records, trimmers)
-      .filter(_ != null)
-//      .coalesce(1)
-      .saveAsTextFile(argsMap("output").asInstanceOf[String])
+      //      .filter(_ != null)
+      //      .coalesce(1)
+      .saveAsTextFile(tempOutput)
 
     session.close
+    concatenateFiles(tempOutput, output)
   }
 
   @tailrec
@@ -51,7 +57,29 @@ class SingleEndMode extends Mode {
     else {
       //            applyTrimmer(trimmers.head(records), trimmers.tail)
       //      applyTrimmer(records.map(f => trimmers.head(Array(f))(0)).filter(f => f != null), trimmers.tail)
-      applyTrimmer(records.map(trimmers.head.processSingle(_)), trimmers.tail)
+      applyTrimmer(records.map(trimmers.head.processSingle(_))
+        .filter(_ != null)
+        , trimmers.tail)
     }
+  }
+
+  private def concatenateFiles(tempOutput: String, output: String): Unit = {
+    val pattern: Regex = "part-\\d+".r
+    val dir = FileSystems.getDefault.getPath(tempOutput)
+    val sw = new BufferedWriter(new FileWriter(output + "/out.fastq"))
+    Files.list(dir).iterator().asScala
+      .filter(p => pattern.matches(p.getFileName.toString))
+      .foreach(p => {
+        val reader = new BufferedReader(new FileReader(p.toString))
+        var line: String = reader.readLine()
+        while (line != null) {
+          sw.write(line)
+          sw.newLine()
+          line = reader.readLine()
+        }
+        reader.close()
+        sw.flush()
+      })
+    sw.close()
   }
 }
